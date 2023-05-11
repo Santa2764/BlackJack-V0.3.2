4306bc01afbdef33b4b61ae42873e11e9d25dcd9
Mikle <127769191+Santa2764@users.noreply.github.com>
2023-05-11 04:00:23 +0100

    Add files via upload

---
+++ BlackJack (V0.3.2)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace BlackJack__V0._3._2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Black Hohol Jack!";
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Game game = new Game("Я");
+            game.Start();
+
+        }
+    }
+}
